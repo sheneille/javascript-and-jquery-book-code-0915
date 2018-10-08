c18cf6f2ec11c0fea20e46186a87f5f11598d45a
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-08 21:49:59 +0530

    c01

--- c01/js/add-content.js
+++ c01/js/add-content.js
@@ -1,7 +1,10 @@
+/* This script displays a greeting message to the user based on the current time*/
+
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
+//Display greeting based on current time
 if (hourNow > 18) {
     greeting = 'Good evening!';
 } else if (hourNow > 12) {
