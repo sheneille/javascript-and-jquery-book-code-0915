d198e634c471e6cddeabf044407159dae8df8556
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-10 17:27:51 +0530

    remove solutions from C02

--- c02/js/shorthand-variable.js
+++ c02/js/shorthand-variable.js
@@ -1,7 +1,5 @@
 // METHOD 1
-var price = 5;
-var quantity = 14;
-var total = price * quantity;
+
 
 /* METHOD 2
 var price, quantity, total;
@@ -16,10 +14,10 @@ var total = price * quantity;
 */
 
 // Write the total into the element with id of cost
-var el = document.getElementById('cost');  // Gets the element with an id of cost
-el.textContent = '$' + total;              // Replaces the content of this element
+ // Gets the element with an id of cost
+    // Replaces the content of this element
 
-/* 
+/*
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on line 20, but note the security issues on p228-231
 el.innerHTML = '$' + total;
