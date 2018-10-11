1749abe46bcd7da6f1675f71aa9af59aaaebd6c9
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-11 22:33:46 +0530

    complete c02 2 files

--- c02/js/numeric-variable.js
+++ c02/js/numeric-variable.js
@@ -1,13 +1,18 @@
 // Create three variables to store the information needed.
-
+var price;
+var quantity;
+var total;
 
 // Assign values to the price and quantity variables.
+price = 15;
+quantity = 20;
 
 // Calculate the total by multiplying the price by quantity.
-
+total = price * quantity
 
 // Get the element with an id of cost.
-
+var el = document.getElementById('cost');
+el.textContent = '$' + total;
 
 /*
 NOTE: textContent does not work in IE8 or earlier
--- c02/js/string-variable.js
+++ c02/js/string-variable.js
@@ -1,18 +1,22 @@
 // Create variables to hold the name and note text.
-
+var name;
+var text;
 
 // Assign values to these variables.
-
+name = 'sheneille';
+text = 'hello';
 
 // Get the element with an id of name.
+var elName = document.getElementById('name');
 
 // Replace the content of this element.
-
+elName.textContent = name;
 
 // Get the element with an id of note.
+var elText = document.getElementById('note');
 
 // Replace the content of this element.
-
+elText.textContent = 'text';
 
 /*
 NOTE: textContent does not work in IE8 or earlier
