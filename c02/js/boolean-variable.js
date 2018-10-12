066736bc6f52683db6153a8ebb5ec73fac80fe3e
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-12 09:44:13 +0530

    add className to a span Id

--- c02/js/boolean-variable.js
+++ c02/js/boolean-variable.js
@@ -3,13 +3,11 @@ var stock = false;
 var shipping = true;
 
 // Get the element that has an id of stock
-var elStock = document.getElementById('stock');
+var elStock = document.getElementById('stock').classList.add("false");
 
 // Set class name with value of inStock variable
-elStock.textContent = stock;
 
 // Get the element that has an id of shipping
-var elShipping = document.getElementById('shipping');
+var elShipping = document.getElementById('shipping').classList.add("true");
 
 // Set class name with value of shipping variable
-elShipping.textContent = shipping;
