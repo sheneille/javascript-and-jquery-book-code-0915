f0dad553b558abed2e432ed657ea1162e7a83575
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-14 19:38:56 +0530

    work on update array

--- c02/js/update-array.js
+++ c02/js/update-array.js
@@ -1,1 +1,13 @@
-// Create the array and assign it valuesvar colors = ['white', 'black', 'custom'];// Update the third item in the arraycolors[2] = 'beige';// Get the element with an id of colorsvar el = document.getElementById('colors');// Replace element with third item from the arrayel.textContent = colors[2];/* NOTE: textContent does not work in IE8 or earlierYou can use innerHTML on line 10, but note the security issues on p228-231el.innerHTML = colors[2];*/
+// Create the array and assign it valuesvar colors = ['white', 'black', 'custom'];
+var colors;
+colors = ['red', 'yellow', 'blue'];
+
+// Update the third item in the arraycolors[2] = 'beige';
+colors[2] = 'green';
+
+// Get the element with an id of colorsvar el = document.getElementById('colors');
+var el = document.getElementById('colors');
+
+// Replace element with third item from the arrayel.textContent = colors[2];
+el.textContent = colors[2];
+/*NOTE: textContent does not work in IE8 or earlierYou can use innerHTML on line 10, but note the security issues on p228-231el.innerHTML = colors[2];*/
