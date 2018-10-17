92f6fa1010d8fc5ebb47b14d129a380d1dd581aa
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-17 09:30:55 +0530

    work on c03: basic0function and oject-literals

--- c03/js/object-literal.js
+++ c03/js/object-literal.js
@@ -1,9 +1,24 @@
 // Set up the object var hotel = { name : 'Quay', rooms : 40, booked : 25, checkAvailability : function() { return this.rooms - this.booked; // Need "this" because inside function} };
+var hotel = {
+  name: 'Quay';
+  rooms: 240;
+  booked: 40;
+  checkAvailability: function(){
+    return this.rooms - this.booked;
+  }
+}
 
-// Update the HTML var elName = document.getElementById('hotelName'); // Get element elName.textContent = hotel.name; // Update HTML with property of the object
+// Update the HTML var elName = document.getElementById('hotelName');
+var elName = document.getElementById('hotelName');
 
-//var elRooms = document.getElementById('rooms');    // Get element elRooms.textContent = hotel.checkAvailability();   // Update HTML with property of the object
+// Get element elName.textContent = hotel.name;
+elName.textContent = hotel.name;
 
+// Update HTML with property of the object
+var elRooms = document.getElementById('rooms')
+
+//var elRooms = document.getElementById('rooms'); // Get element elRooms.textContent = hotel.checkAvailability(); // Update HTML with property of the object
+elRooms.textContent = hotel.rooms;
 /*
 NOTE: textContent does not work in IE8 or earlier
 You can use innerHTML on lines 13 and 16, but note the security issues on p228-231
