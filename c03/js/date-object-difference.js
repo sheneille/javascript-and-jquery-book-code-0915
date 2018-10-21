7fc828095e3043c41524ec5a419ff8526ec7fd33
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-21 15:22:53 +0530

    work on c03

--- c03/js/date-object-difference.js
+++ c03/js/date-object-difference.js
@@ -1,13 +1,27 @@
-// Create a variable to hold a new Date object (defaults to now)var today = new Date();
+// Create a variable to hold a new Date object (defaults to now)
 
-// Get the year this yearvar year = today.getFullYear();
+var today = new Date();
 
-// Set the date that the company was establishedvar est = new Date('Apr 16, 1996 15:45:55');
+// Get the year this year
 
-// Get difference between then & now in millisecondsvar difference = today.getTime() - est.getTime();
+var year = today.getFullYear();
 
-// Divide by number of milliseconds to get yearsdifference = (difference / 31556900000);
+// Set the date that the company was established
 
-// Create a variable called elMsg to hold the element whose id attribute has a value of messagevar elMsg = document.getElementById('message');
+var est = new Date('Apr 16, 1996 15:45:55');
 
-// Write the message into that element.elMsg.textContent = Math.floor(difference) + ' years of online travel advice';
+// Get difference between then & now in milliseconds
+
+var difference = today.getTime() - est.getTime();
+
+// Divide by number of milliseconds to get years
+
+difference = (difference / 31556900000);
+
+// Create a variable called elMsg to hold the element whose id attribute has a value of message
+
+var elMsg = document.getElementById('message');
+
+// Write the message into that
+
+element.elMsg.textContent = Math.floor(difference) + ' years of online travel advice';
--- c03/js/number-object.js
+++ c03/js/number-object.js
@@ -1,11 +1,23 @@
-// Create a variable to hold the number that will be used var originalNumber = 10.23456;
+// Create a variable to hold the number that will be used
 
-// Create a variable to hold a message that will be shown on the page var msg = '<h2>original number</h2><p>' + originalNumber + '</p>';
+var originalNumber = 10.23456;
 
-// Round the number to 3 decimal places and add it to the msg variable msg += '<h2>3 decimal places</h2><p>' + originalNumber.toFixed(3) + '</p>';
+// Create a variable to hold a message that will be shown on the page
 
-// Round the number to a precisely 3 digits and add it to the msg variable msg += '<h2>3 digits</h2><p>' + originalNumber.toPrecision(3) + '</p>';
+var msg = '<h2>original number</h2><p>' + originalNumber + '</p>';
 
-// Create a variable called el to hold the element whose id attribute has a value of info var el = document.getElementById('info');
+// Round the number to 3 decimal places and add it to the msg variable
 
-// Write the message into that element el.innerHTML = msg;
+msg += '<h2>3 decimal places</h2><p>' + originalNumber.toFixed(3) + '</p>';
+
+// Round the number to a precisely 3 digits and add it to the msg variable
+
+msg += '<h2>3 digits</h2><p>' + originalNumber.toPrecision(3) + '</p>';
+
+// Create a variable called el to hold the element whose id attribute has a value of info
+
+var el = document.getElementById('info');
+
+// Write the message into that element
+
+el.innerHTML = msg;
