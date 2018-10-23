56a9b2cade4d0effe1f8b1a190c226b4e70f9630
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-23 23:07:49 +0530

    remove ans

--- c04/js/comparison-operator-continued.js
+++ c04/js/comparison-operator-continued.js
@@ -1,11 +1,8 @@
-var score1 = 90;     // Round 1 score
-var score2 = 95;     // Round 2 score
-var highScore1 = 75; // Round 1 high score
-var highScore2 = 95; // Round 2 high score
+// Round 1 score - var score1 = 90;
+ // Round 2 score - var score2 = 95;
+ // Round 1 high score - var highScore1 = 75;
+ // Round 2 high score - var highScore2 = 95;
 
-// Check if scores are higher than current high scores
-var comparison = (score1 + score2) > (highScore1 + highScore2);
+// Check if scores are higher than current high scores var comparison = (score1 + score2) > (highScore1 + highScore2);
 
-// Write the message into the page
-var el = document.getElementById('answer');
-el.innerHTML = 'New high score: ' + comparison;
+// Write the message into the page var el = document.getElementById('answer'); el.innerHTML = 'New high score: ' + comparison;
