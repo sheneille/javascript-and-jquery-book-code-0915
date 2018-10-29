86bff6ecca10ae3a96acdde68759fd99d86ab648
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-29 23:09:39 +0530

    c04

--- c04/js/do-while-loop.js
+++ c04/js/do-while-loop.js
@@ -1,1 +1,13 @@
-//var i = 1;       // Set counter to 1var msg = '';    // Message// Store 5 times table in a variabledo {  msg += i + ' x 5 = ' + (i * 5) + '<br />';  i++;} while (i < 1); // Note how this is already 1 and it still runsdocument.getElementById('answer').innerHTML = msg;
+//Set counter to 1 and msg = blank
+var i = 1;
+var msg = '';
+
+// Store 5 times table in a variable
+
+do {
+  msg += i + ' x 5 = ' + (i * 5) + '<br />';  i++;
+}
+while (i < 1);
+
+// Note how this is already 1 and it still runs
+document.getElementById('answer').innerHTML = msg;
