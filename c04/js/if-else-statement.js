b71a1c04886b872e1d38c3fc2d322caf454f564e
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-29 22:52:38 +0530

    c04

--- c04/js/if-else-statement.js
+++ c04/js/if-else-statement.js
@@ -1,6 +1,17 @@
-// Pass mark  var pass = 50;
-// Current score  var score = 75;
-// Message  var msg;
+//Store pass marks as 50 and score as 75 and a Message
+var pass = 50;
+var score = 75;
+var msg;
 
-// Select message to write based on score if (score > pass) {msg = 'Congratulations, you passed!'; } else {msg = 'Have another go!'; }
-//var el = document.getElementById('answer'); el.textContent = msg;
+// Select message to write based on score
+
+if (score > pass) {
+  msg = 'Congratulations, you passed!';
+}
+else {
+  msg = 'Have another go!';
+}
+
+//Write the element with the id answer
+var el = document.getElementById('answer');
+el.textContent = msg;
