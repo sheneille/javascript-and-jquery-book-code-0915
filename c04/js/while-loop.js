9224d3cbac28172272f38092d4ca61e8062f4d81
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-29 23:06:57 +0530

    c04

--- c04/js/while-loop.js
+++ c04/js/while-loop.js
@@ -1,1 +1,11 @@
-//var i = 1;       // Set counter to 1var msg = '';    // Message// Store 5 times table in a variablewhile (i < 10) {  msg += i + ' x 5 = ' + (i * 5) + '<br />';  i++;}document.getElementById('answer').innerHTML = msg;
+//Set counter to 1 and msg = blank
+var i = 1;
+var msg = '';
+
+// Store 5 times table in a variable
+
+while (i < 10) {
+  msg += i + ' x 5 = ' + (i * 5) + '<br />';  i++;
+}
+
+document.getElementById('answer').innerHTML = msg;
