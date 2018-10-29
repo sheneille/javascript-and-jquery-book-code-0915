55a97014392a9877b34f71795813112cdfb678a5
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-29 22:39:38 +0530

    c04

--- c04/js/if-statement.js
+++ c04/js/if-statement.js
@@ -1,1 +1,14 @@
-//var score = 75;     // Scorevar msg;            // Messageif (score >= 50) {  // If score is 50 or higher  msg = 'Congratulations!';  msg += ' Proceed to the next round.'; }var el = document.getElementById('answer');el.textContent = msg;
+//Store 75 as score and a blank Message
+var score = 75;
+var msg = ' ';
+
+//If score is 50 or higher add Congratulations and Proceed to the next round in the message;
+
+if (score >= 50) { msg = 'Congratulations!';
+  msg += ' Proceed to the next round.';
+}
+
+//Create an element by getting the id of answer and then write the message on the element
+
+var el = document.getElementById('answer');
+el.textContent = msg;
