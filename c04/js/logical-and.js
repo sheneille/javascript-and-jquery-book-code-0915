81b6561718759cff720cda7b5a6e57fec60cdecd
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-29 22:24:05 +0530

    c04

--- c04/js/logical-and.js
+++ c04/js/logical-and.js
@@ -1,10 +1,18 @@
-// Round 1 score  var score1 = 8;
-// Round 2 score  var score2 = 8;
-// Round 1 pass mark  var pass1 = 6; 
-// Round 2 pass mark  var pass2 = 6; 
+// Store values for round 1 = score1, score2 and round 2 = pass1 and pass2 as 8, 8, 6 and 6 respectively
 
-// Check whether user passed both rounds, store result in variable var passBoth = (score1 >= pass1) && (score2 >= pass2);
+var score1 = 8;
+var score2 = 8;
+var pass1 = 6;
+var pass2 = 6;
 
-// Create message var msg = 'Both rounds passed: ' + passBoth;
+// Check whether user passed both rounds, store result in variable
 
-// Write the message into the page var el = document.getElementById('answer'); el.innerHTML = msg;
+var passBoth = (score1 >= pass1) && (score2 >= pass2);
+
+// Create message
+
+var msg = 'Both rounds passed: ' + passBoth;
+
+// Write the message into the page
+var el = document.getElementById('answer');
+el.innerHTML = msg;
--- c04/js/logical-or-logical-not.js
+++ c04/js/logical-or-logical-not.js
@@ -1,10 +1,20 @@
-// Round 1 score  var score1 = 8;
-// Round 2 score  var score2 = 8;
-// Round 1 pass mark  var pass1 = 6;
-// Round 2 pass mark  var pass2 = 6;
+// Store values for round 1 = score1, score2 and round 2 = pass1 and pass2 as 8, 8, 6 and 6 respectively
+
+var score1 = 8;
+var score2 = 8;
+var pass1 = 6;
+var pass2 = 6;
 
 // Check whether user passed one of the two rounds, store result in variable var minPass = (score1 >= pass1) || (score2 >= pass2);
 
+var minPass = (score1 >= pass1) || (score2 >= pass2);
+
 // Create message var msg = 'Resit required: ' + !minPass;
 
-// Write the message into the page var el = document.getElementById('answer'); el.innerHTML = msg;
+var msg = 'Resit required: ' + !minPass;
+
+// Write the message into the page
+
+var el = document.getElementById('answer');
+
+el.innerHTML = msg;
