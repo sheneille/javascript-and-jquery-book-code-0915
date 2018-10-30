94e63bfb9995d618b0e443cf3c9ebe54c2fd8858
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-30 20:11:29 +0530

    c05

--- c05/js/node-list.js
+++ c05/js/node-list.js
@@ -1,8 +1,7 @@
-//var hotItems = document.querySelectorAll('li.hot'); // Store NodeList in array
-//if (hotItems.length > 0) {                          // If it contains items
+var hotItems = document.querySelectorAll('li.hot');
 
-  //for (var i = 0; i < hotItems.length; i++) {       // Loop through each item
-    //hotItems[i].className = 'cool';         // Change value of class attribute
-  //}
-
-//}
+if(hotItems.length > 0){
+  for(i = 0; i < hotItems.length; i++){
+    hotItems[i].className = 'cool';
+  }
+}
