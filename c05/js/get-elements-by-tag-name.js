7e5d8dbaeff8cdd8ce9364798dd1091f3a03f678
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-30 19:48:31 +0530

    c05

--- c05/js/get-elements-by-tag-name.js
+++ c05/js/get-elements-by-tag-name.js
@@ -1,1 +1,6 @@
-//var elements = document.getElementsByTagName('li'); // Find <li> elementsif (elements.length > 0) {  // If 1 or more are found  var el = elements[0];     // Select the first one using array syntax  el.className = 'cool';    // Change the value of the class attribute}
+var elements = document.getElementsByTagName('li');
+
+if (elements.length > 2){
+  var el = elements[0];
+  el.className = 'cool';
+}
