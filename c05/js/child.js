a31e1f44035530330d2f6d77b0dedebe6b4d9757
Sheneille Patil <sheneillepatil@gmail.com>
2018-10-31 08:29:07 +0530

    c05

--- c05/js/child.js
+++ c05/js/child.js
@@ -1,1 +1,7 @@
-// Select the starting point and find its children.var startItem = document.getElementsByTagName('ul')[0];var firstItem = startItem.firstChild;var lastItem = startItem.lastChild;// Change the values of the children's class attributes.firstItem.className = 'complete';lastItem.className = 'cool';
+// Select the starting point and find its children
+var startItem = document.getElementsByTagName('ul')[0];
+var firstItem = startItem.firstChild;
+var lastItem = startItem.lastChild;
+
+firstItem.setAttribute('class', 'complete');
+lastItem.setAttribute('class', 'cool');
--- c05/js/node-value.js
+++ c05/js/node-value.js
@@ -1,1 +1,4 @@
-//var itemTwo = document.getElementById('two');  // Get second list itemvar elText  = itemTwo.firstChild.nodeValue;    // Get its text contentelText = elText.replace('pine nuts', 'kale');  // Change pine nuts to kaleitemTwo.firstChild.nodeValue = elText;         // Update the list item
+var itemTwo = document.getElementById('two');
+var elText = itemTwo.firstChild.nodeValue;
+elText = elText.replace('pine nuts', 'kale');
+itemTwo.firstChild.nodeValue = elText;
