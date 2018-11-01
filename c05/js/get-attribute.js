a4274a001dff31514daef22537a7e85a1989c783
Sheneille Patil <sheneillepatil@gmail.com>
2018-11-01 07:25:14 +0530

    c05

--- c05/js/get-attribute.js
+++ c05/js/get-attribute.js
@@ -1,7 +1,11 @@
 //var firstItem = document.getElementById('one'); // Get first list item
-//if (firstItem.hasAttribute('class')) {          // If it has class attribute
-  //var attr = firstItem.getAttribute('class');   // Get the attribute
+var firstItem = document.getElementById('one');
 
+//if (firstItem.hasAttribute('class')) {          // If it has class attribute
+
+if(firstItem.hasAttribute('class')){
+  //var attr = firstItem.getAttribute('class');   // Get the attribute
+  var attr = firstItem.getAttribute('class');
   // Add the value of the attribute after the list
-  //var el = document.getElementById('scriptResults');
-  //el.innerHTML = '<p>The first item has a class name: ' + attr + '</p>';}
+  var el = document.getElementById('scriptResults');
+  el.innerHTML = '<p>The first item has a class name: ' + attr + '</p>';}
