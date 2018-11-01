ffaef65670f7b97975b5ddfe457ade6db489b2fb
Sheneille Patil <sheneillepatil@gmail.com>
2018-11-01 06:40:22 +0530

    c05

--- c05/js/inner-text-and-text-content.js
+++ c05/js/inner-text-and-text-content.js
@@ -1,1 +1,18 @@
-//var firstItem = document.getElementById('one'); // Find first list itemvar showTextContent = firstItem.textContent;    // Get value of textContentvar showInnerText = firstItem.innerText;        // Get value of innerText// Show the content of these two properties at the end of the listvar msg = '<p>textContent: ' + showTextContent + '</p>';    msg += '<p>innerText: ' + showInnerText + '</p>';var el = document.getElementById('scriptResults');el.innerHTML = msg;firstItem.textContent = 'sourdough bread';       // Update the first list item
+// Find first list item
+var firstItem = document.getElementById('one');
+
+// Get value of textContent
+var showTextContent = firstItem.textContent;
+
+// Get value of innerText
+var showInnerText = firstItem.innerText;
+
+// Show the content of these two properties at the end of the list
+var msg = '<p>textContent: ' + showTextContent + '</p>';
+msg += '<p>innerText: ' + showInnerText + '</p>';
+
+var el = document.getElementById('scriptResults');
+el.innerHTML = msg;
+
+// Update the first list item
+firstItem.textContent = 'sourdough bread';
