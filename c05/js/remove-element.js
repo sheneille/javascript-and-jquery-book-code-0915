242b1b9e99d008aff0ef4b75003d3586d7a07b3b
Sheneille Patil <sheneillepatil@gmail.com>
2018-11-01 07:18:58 +0530

    c05

--- c05/js/remove-element.js
+++ c05/js/remove-element.js
@@ -1,5 +1,6 @@
 // Store the element to be removed in a variable.
-var containerEl = document.getElementsByTagName('ul')[0];
+var removeEl = document.getElementsByTagName('li')[2];
 
+var containerEl = removeEl.parentNode;
 // Remove the element.
 containerEl.removeChild(removeEl);
